fdbc4f6506d08eb3695dd049b591bbdd5e74cff7
joshtrigger <lugjosh@gmail.com>
2019-08-21 19:09:38 +0300

    fixed interplotaion bug

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -5,107 +5,110 @@ const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.TP_EMAIL,
-    pass: process.env.TP_PASSWORD
-  }
+    service: 'gmail',
+    auth: {
+        user: process.env.TP_EMAIL,
+        pass: process.env.TP_PASSWORD
+    }
 });
 
 function jwtSignature(payload) {
-  const token = jwt.sign(payload, process.env.SECRETKEY, {
-    expiresIn: '1h'
-  });
-  return token;
+    const token = jwt.sign(payload, process.env.SECRETKEY, {
+        expiresIn: '1h'
+    });
+    return token;
 }
 const signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(409).json({
-        error: "User already exists"
-      });
-    } else {
-      user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        email: req.body.email,
-        password: hash
-      });
-    }
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+            return res.status(409).json({
+                error: "User already exists"
+            });
+        } else {
+            user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hash
+            });
+        }
 
-    user
-      .save()
-      .then(() => {
-        res.status(201).json({
-          message: `Welcome ${
+        user
+            .save()
+            .then(() => {
+                res.status(201).json({
+                    message: `Welcome ${
             req.body.username
           }, you have been successfully registered`
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: err
-        });
-      });
-  });
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: err
+                });
+            });
+    });
 };
 
 const login = (req, res, next) => {
-  if (req.body.password === '' || req.body.password === null) {
-    return res.status(400).json({
-      error: {
-        message: 'password field cannot be blank'
-      }
-    });
-  }
-  User.findOne({
-    email: req.body.email
-  })
-    .exec()
-    .then(user => {
-      bcrypt.compare(req.body.password, user.password, (err, response) => {
-        if (response) {
-          const payload = {
-            userId: user._id,
-            email: user.email,
-            username: user.username
-          };
-          const token = jwtSignature(payload);
-          res.status(200).json({
-            message: 'you have logged in successfully',
-            token: token
-          });
-        } else {
-          res.status(401).json({
+    if (req.body.password === '' || req.body.password === null) {
+        return res.status(400).json({
             error: {
-              message: 'Authentication failure'
+                message: 'password field cannot be blank'
             }
-          });
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: 'User does not exist'
-      });
-    });
+        });
+    }
+    User.findOne({
+            email: req.body.email
+        })
+        .exec()
+        .then(user => {
+            bcrypt.compare(req.body.password, user.password, (err, response) => {
+                if (response) {
+                    const payload = {
+                        userId: user._id,
+                        email: user.email,
+                        username: user.username
+                    };
+                    const token = jwtSignature(payload);
+                    res.status(200).json({
+                        message: 'you have logged in successfully',
+                        token: token
+                    });
+                } else {
+                    res.status(401).json({
+                        error: {
+                            message: 'Authentication failure'
+                        }
+                    });
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'User does not exist'
+            });
+        });
 };
 
 const passwordResetRequest = (req, res, next) => {
-  User.findOne({
-    email: req.body.email
-  })
-    .exec()
-    .then(result => {
-      if (result) {
-        const payload = { userId: result._id, email: result.email };
-        const token = jwtSignature(payload);
-        const url = `${process.env.BASE_URL}/reset_password/${token}`;
-        const mailOptions = {
-          from: process.env.TP_EMAIL,
-          to: `${result.email}`,
-          subject: 'Password Reset',
-          html: `
+    User.findOne({
+            email: req.body.email
+        })
+        .exec()
+        .then(result => {
+            if (result) {
+                const payload = {
+                    userId: result._id,
+                    email: result.email
+                };
+                const token = jwtSignature(payload);
+                const url = `${process.env.BASE_URL}user/reset_password/${token}`;
+                const mailOptions = {
+                    from: process.env.TP_EMAIL,
+                    to: `${result.email}`,
+                    subject: 'Password Reset',
+                    html: `
                     <h2>My Shop Password reset request</h2>
                     <p>
                         Hello ${result.username},<br>
@@ -116,95 +119,93 @@ const passwordResetRequest = (req, res, next) => {
                         <a href=${url}>Click here</a><br>
                     </p>
                 `
-        };
+                };
 
-        transporter.sendMail(mailOptions, (err, info) => {
-          if (err) {
+                transporter.sendMail(mailOptions, (err, info) => {
+                    if (err) {
+                        res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        res.status(200).json(info);
+                    }
+                });
+            } else {
+                res.status(500).json({
+                    error: ''
+                });
+            }
+        })
+        .catch(err => {
             res.status(500).json({
-              error: err
+                error: err
             });
-          } else {
-            res.status(200).json(info);
-          }
         });
-      } else {
-        res.status(500).json({
-          error: ''
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
 };
 
 const resetPassword = (req, res, next) => {
-  const id = jwt.verify(req.params.userId, process.env.SECRETKEY);
-  const a = req.body.newPassword;
-  const b = req.body.confirmPassword;
-
-  User.findById({
-    _id: id
-  })
-    .exec()
-    .then(result => {
-      if (result && a === b) {
-        //update the password form here
-        bcrypt.hash(a, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
+    const id = jwt.verify(req.params.signature, process.env.SECRETKEY);
+    const a = req.body.newPassword;
+    const b = req.body.confirmPassword;
+    console.log(id.userId)
+    User.findById({
+            _id: id.userId
+        })
+        .exec()
+        .then(result => {
+            if (result && a === b) {
+                //update the password form here
+                bcrypt.hash(a, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        User.findOneAndUpdate({
+                                _id: id.userId
+                            }, {
+                                password: hash
+                            },
+                            (err, data) => {
+                                if (err) {
+                                    res.status(500).json(err);
+                                } else {
+                                    res.status(200).json({
+                                        message: 'Password has been reset successfully'
+                                    });
+                                }
+                            }
+                        );
+                    }
+                });
+            } else if (a !== b) {
+                return res.status(400).json({
+                    error: {
+                        message: 'Passwords do not match'
+                    }
+                });
+            } else if (a !== '' || b !== '') {
+                return res.status(400).json({
+                    error: {
+                        message: 'Fields cannot be blank'
+                    }
+                });
+            } else {
+                return res.status(400).json({
+                    error: 'user does not exist'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
             });
-          } else {
-            User.findOneAndUpdate(
-              {
-                _id: id
-              },
-              {
-                password: hash
-              },
-              (err, data) => {
-                if (err) {
-                  res.status(500).json(err);
-                } else {
-                  res.status(200).json({
-                    message: 'Password has been reset successfully'
-                  });
-                }
-              }
-            );
-          }
         });
-      } else if (a !== b) {
-        return res.status(400).json({
-          error: {
-            message: 'Passwords do not match'
-          }
-        });
-      } else if (a !== '' || b !== '') {
-        return res.status(400).json({
-          error: {
-            message: 'Fields cannot be blank'
-          }
-        });
-      } else {
-        return res.status(400).json({
-          error: 'user does not exist'
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
 };
 
 module.exports = {
-  signup,
-  login,
-  passwordResetRequest,
-  resetPassword
+    signup,
+    login,
+    passwordResetRequest,
+    resetPassword
 };
